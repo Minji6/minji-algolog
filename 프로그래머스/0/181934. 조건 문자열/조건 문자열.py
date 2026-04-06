1f4ca2c350c8ef9b2ae490b1b6ae73185484620a
김민지 <105301353+Minji6@users.noreply.github.com>
2026-04-06 23:56:40 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,14 @@
+def solution(ineq, eq, n, m):
+    answer = 0
+    
+    if ineq == ">" and eq == "=":
+        return int(n >= m)
+    
+    elif ineq == "<" and eq == "=":
+        return int(n <= m)
+    
+    elif ineq == ">" and eq == "!":
+        return int(n > m)
+    
+    else:
+        return int(n < m)
