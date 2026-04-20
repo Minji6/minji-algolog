a09cc4d7b2f6deb98ab6bbb8500115a57f5b2ebd
김민지 <105301353+Minji6@users.noreply.github.com>
2026-04-20 22:18:56 +0900

    [level 0] Title: 2의 영역, Time: 8.17 ms, Memory: 14.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    
+    for i in range(len(arr)):
+        if arr[i] == 2:
+            answer.append(i)
+    
+    if not answer:
+        return [-1]
+    return arr[answer[0]:answer[-1]+1]
