ba2170abefbabe7eab4ccbb91c235e9bd4b3fa99
김민지 <105301353+Minji6@users.noreply.github.com>
2026-06-01 21:08:22 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.13 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -0,0 +1,25 @@
+def solution(n):
+    answer = [[0] * n for _ in range(n)]
+    dr = [0, 1, 0, -1] # 우, 하, 좌, 상
+    dc = [1, 0, -1, 0]
+    
+    r, c = 0, 0
+    d = 0
+    num = 1
+    
+    for num in range(1, n*n + 1):
+        answer[r][c] = num
+        num += 1
+        
+        nr = r + dr[d]
+        nc = c + dc[d]
+        
+        if nr < 0 or nr >= n or nc < 0 or nc >= n or answer[nr][nc] != 0:
+            d = (d+1) % 4
+            nr = r + dr[d]
+            nc = c + dc[d]
+            
+        r, c = nr, nc
+    return answer
+            
+        
